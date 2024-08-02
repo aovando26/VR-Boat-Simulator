89720bd8491993955a06653eae6e39a9ce62aeeb
aovando <aovandodevsxr@gmail.com>
2024-08-01 20:56:28 -0400

    hud interface

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,7 +14,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,18 +28,18 @@ public class UIManager : MonoBehaviour
     void UpdateWindVectorUI()
     {
         float AngleInRad = Mathf.Atan2(WindManager.instance.CurrentTrueWind.y, WindManager.instance.CurrentTrueWind.x);
-        WindVectorUI.transform.rotation = Quaternion.Euler(0,0,AngleInRad*Mathf.Rad2Deg);
+        WindVectorUI.transform.rotation = Quaternion.Euler(0, 0, AngleInRad * Mathf.Rad2Deg);
 
     }
 
     void UpdateSailDirectionUI()
     {
-        SailDirectionUI.transform.rotation = Quaternion.Euler(0,0,-BoatManager.Player.Sail.transform.localRotation.eulerAngles.y + 90 - BoatManager.Player.transform.localRotation.eulerAngles.y);
+        SailDirectionUI.transform.rotation = Quaternion.Euler(0, 0, -BoatManager.Player.Sail.transform.localRotation.eulerAngles.y + 90 - BoatManager.Player.transform.localRotation.eulerAngles.y);
     }
 
     void UpdateApparentWindUI()
     {
         float AngleInRad = Mathf.Atan2(BoatManager.Player.ApparentWind.y, BoatManager.Player.ApparentWind.x);
-        ApparentWindUI.transform.rotation = Quaternion.Euler(0,0,AngleInRad*Mathf.Rad2Deg);
+        ApparentWindUI.transform.rotation = Quaternion.Euler(0, 0, AngleInRad * Mathf.Rad2Deg);
     }
 }
