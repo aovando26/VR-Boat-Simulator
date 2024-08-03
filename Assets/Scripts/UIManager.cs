b302ba97a824046654910d833a3368495e612efe
aovando <aovandodevsxr@gmail.com>
2024-08-03 16:05:08 -0400

    vector3 to vector2

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,8 +9,7 @@ public class UIManager : MonoBehaviour
     public GameObject WindVectorUI;
     public GameObject SailDirectionUI;
     public GameObject ApparentWindUI;
-
-
+    public GameObject player; 
     // Start is called before the first frame update
     void Start()
     {
@@ -28,18 +27,29 @@ public class UIManager : MonoBehaviour
     void UpdateWindVectorUI()
     {
         float AngleInRad = Mathf.Atan2(WindManager.instance.CurrentTrueWind.y, WindManager.instance.CurrentTrueWind.x);
+        Debug.Log(AngleInRad);
         WindVectorUI.transform.rotation = Quaternion.Euler(0, 0, AngleInRad * Mathf.Rad2Deg);
 
+        // the y axis is being passed in the x coordinate then Euler Angles is applied with respect to the player transform
+        WindVectorUI.transform.Rotate(player.transform.eulerAngles.y, 0, 0);
+        //Debug.Log(WindVectorUI);
     }
 
     void UpdateSailDirectionUI()
     {
         SailDirectionUI.transform.rotation = Quaternion.Euler(0, 0, -BoatManager.Player.Sail.transform.localRotation.eulerAngles.y + 90 - BoatManager.Player.transform.localRotation.eulerAngles.y);
+
+        // rotates around x axis depending on y
+        SailDirectionUI.transform.Rotate(player.transform.eulerAngles.y, 0, 0);
+        //Debug.Log(SailDirectionUI);
     }
 
     void UpdateApparentWindUI()
     {
         float AngleInRad = Mathf.Atan2(BoatManager.Player.ApparentWind.y, BoatManager.Player.ApparentWind.x);
+        Debug.Log(AngleInRad);
         ApparentWindUI.transform.rotation = Quaternion.Euler(0, 0, AngleInRad * Mathf.Rad2Deg);
+        ApparentWindUI.transform.Rotate(player.transform.eulerAngles.y, 0, 0);
+        //Debug.Log(ApparentWindUI);
     }
 }
